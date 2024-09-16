b34bb8992a72a326ba986cd90d5270bb36cf6a19
Lethal15004 <Phamvahuy2004@gmail.com>
2024-09-16 22:39:27 +0700

    Bài tập 3

--- app/src/main/java/com/example/learnandroid/RegisterActivity.java
+++ app/src/main/java/com/example/learnandroid/RegisterActivity.java
@@ -3,8 +3,6 @@ package com.example.learnandroid;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,25 +13,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    private TextView createNewAccount;
-    private AppCompatButton btnLogin;
+public class RegisterActivity extends AppCompatActivity {
+    private TextView alreadyHaveAccount;
+    private AppCompatButton btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        createNewAccount=findViewById(R.id.createNewAccount);
-        btnLogin=findViewById(R.id.btnLogin);
-        createNewAccount.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_register);
+        alreadyHaveAccount=findViewById(R.id.alreadyHaveAccount);
+        btnRegister=findViewById(R.id.btnRegister);
+        alreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+                startActivity(new Intent(RegisterActivity.this,MainActivity.class));
             }
         });
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this,
+                Toast.makeText(RegisterActivity.this,
                         "Mã sinh viên: 22115053122117 " + "\nHọ tên: Phạm Lê Văn Huy ",
                         Toast.LENGTH_SHORT).show();
             }
