c2c63545decbd97980e736da7a8b7eddaae2173f
Lethal15004 <Phamvahuy2004@gmail.com>
2024-10-13 22:56:11 +0700

    Làm bài Tập 4

--- app/src/main/java/com/example/learnandroid/MainActivity.java
+++ app/src/main/java/com/example/learnandroid/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.learnandroid;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,28 +20,52 @@ import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView createNewAccount;
-    private AppCompatButton btnLogin;
+    FrameLayout frameLayout;
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        createNewAccount=findViewById(R.id.createNewAccount);
-        btnLogin=findViewById(R.id.btnLogin);
-        createNewAccount.setOnClickListener(new View.OnClickListener() {
+        frameLayout=(FrameLayout)findViewById(R.id.frameLayout);
+        tabLayout=(TabLayout)findViewById(R.id.tabLayout);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,new FirstFragment()).addToBackStack(null).commit();
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+            public void onTabSelected(TabLayout.Tab tab) {
+                Fragment fragment=null;
+                switch(tab.getPosition()){
+                    case 0:
+                        fragment= new FirstFragment();
+                        break;
+                    case 1:
+                        fragment= new SecondFragment();
+                        break;
+                    case 2:
+                        fragment=new ThirdFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,fragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
             }
-        });
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this,
-                        "Mã sinh viên: 22115053122117 " + "\nHọ tên: Phạm Lê Văn Huy ",
-                        Toast.LENGTH_SHORT).show();
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
         EdgeToEdge.enable(this);
